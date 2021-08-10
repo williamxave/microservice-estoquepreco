9e5eb858b6e1afa73fde1abd97bf755ca663e081
William Bohn <williamxavero@gmail.com>
2021-08-10 08:30:39 -0300

    setup do projeto

---
+++ src/test/java/com/microservice/estoquepreco/EstoqueprecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.estoquepreco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueprecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
